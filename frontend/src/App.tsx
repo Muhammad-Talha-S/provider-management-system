f311fd5d53c4a2946c1b010792b897205517bab2
usman280 <usmanzafar280@gmail.com>
2025-12-30 23:21:41 +0100

    Feat: Basic app create and tailwind configured

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-red-300 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">Tailwind v4 + Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
